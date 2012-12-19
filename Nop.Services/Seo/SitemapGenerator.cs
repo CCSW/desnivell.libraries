b07a07f1509efbbdb4c155f9ba809f8a1fd1342e
dolomita28 <carlos.mas28@gmail.com>
2012-12-19 11:18:49 +0100

    No generaba las rutas de sitemap que se suben a Google correctamente, ya que yo cambié el mapa de rutas para que fueran más SEO friendly.

--- Nop.Services/Seo/SitemapGenerator.cs
+++ Nop.Services/Seo/SitemapGenerator.cs
@@ -64,7 +64,7 @@ namespace Nop.Services.Seo
             foreach (var category in categories)
             {
                 //TODO add a method for getting URL (use routing because it handles all SEO friendly URLs)
-                var url = string.Format("{0}c/{1}/{2}", _webHelper.GetStoreLocation(false), category.Id, category.GetSeName());
+                var url = string.Format("{0}{2}-c-{1}", _webHelper.GetStoreLocation(false), category.Id, category.GetSeName());
                 var updateFrequency = UpdateFrequency.Weekly;
                 var updateTime = category.UpdatedOnUtc;
                 WriteUrlLocation(url, updateFrequency, updateTime);
@@ -79,7 +79,7 @@ namespace Nop.Services.Seo
             foreach (var manufacturer in manufacturers)
             {
                 //TODO add a method for getting URL (use routing because it handles all SEO friendly URLs)
-                var url = string.Format("{0}m/{1}/{2}", _webHelper.GetStoreLocation(false), manufacturer.Id, manufacturer.GetSeName());
+                var url = string.Format("{0}{2}-m-{1}", _webHelper.GetStoreLocation(false), manufacturer.Id, manufacturer.GetSeName());
                 var updateFrequency = UpdateFrequency.Weekly;
                 var updateTime = manufacturer.UpdatedOnUtc;
                 WriteUrlLocation(url, updateFrequency, updateTime);
@@ -92,7 +92,7 @@ namespace Nop.Services.Seo
             foreach (var product in products)
             {
                 //TODO add a method for getting URL (use routing because it handles all SEO friendly URLs)
-                var url = string.Format("{0}p/{1}/{2}", _webHelper.GetStoreLocation(false), product.Id, product.GetSeName());
+                var url = string.Format("{0}{2}-p-{1}", _webHelper.GetStoreLocation(false), product.Id, product.GetSeName());
                 var updateFrequency = UpdateFrequency.Weekly;
                 var updateTime = product.UpdatedOnUtc;
                 WriteUrlLocation(url, updateFrequency, updateTime);
@@ -105,7 +105,8 @@ namespace Nop.Services.Seo
             foreach (var topic in topics)
             {
                 //TODO add a method for getting URL (use routing because it handles all SEO friendly URLs)
-                var url = string.Format("{0}t/{1}", _webHelper.GetStoreLocation(false), topic.SystemName.ToLowerInvariant());
+                //var url = string.Format("{0}t/{1}", _webHelper.GetStoreLocation(false), topic.SystemName.ToLowerInvariant());
+                var url = string.Format("{1}", _webHelper.GetStoreLocation(false), topic.SystemName.ToLowerInvariant());
                 var updateFrequency = UpdateFrequency.Weekly;
                 var updateTime = DateTime.UtcNow;
                 WriteUrlLocation(url, updateFrequency, updateTime);
